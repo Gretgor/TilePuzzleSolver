571114ccedd2687499058f9dbd80b5e6687fc04e
unknown <gretgor42@gmail.com>
2023-06-08 23:51:09 -0300

    feat(sample_problem1.py): add a sample problem

---
+++ sample_problem1.py
@@ -0,0 +1,100 @@
+from main_class import tileProblem, singleTile
+from common_divisor import commonDivisorProblem
+
+grid = [
+    '#############',
+    '##---###---##',
+    '##-#-----#-##',
+    '##-#-###-#-##',
+    '##-#-----#-##',
+    '#-----#-----#',
+    '#-###-#-###-#',
+    '#-----#-----#',
+    '##-#-----#-##',
+    '##-#-###-#-##',
+    '##-#-----#-##',
+    '##---###---##',
+    '#############'    
+    ]
+
+usable_grid = []
+for line in grid:
+    usable_grid.append(list(line))
+
+tiles = [
+        [
+        '##-',
+        '#--',
+        '#--',
+        '#--',
+        '###'
+        ],
+        [
+        '##',
+        '#-',
+        '#-',
+        '#-',
+        '##',
+        '-#'
+        ],
+        [
+        '-#-',
+        '-#-',
+        '-#-',
+        '-#-',
+        '###',
+        '--#'
+        ],
+        [
+        '-##',
+        '-#-',
+        '-#-',
+        '-#-',
+        '###'
+        ],
+        [
+        '#---',
+        '#---',
+        '####',
+        '-#--',
+        '-#--'
+        ],
+        [
+        '-#---',
+        '-#---',
+        '-#---',
+        '#####'
+        ],
+        [
+        '-###',
+        '-#--',
+        '-#--',
+        '##--',
+        '-#--'
+        ],
+        [
+        '##-',
+        '#--',
+        '###',
+        '-#-',
+        '-#-'
+        ],
+        [
+        '-#-',
+        '###',
+        '#--',
+        '###'
+        ]
+    ]
+
+usable_pieces = []
+for piece in tiles:
+    new_piece = []
+    for line in piece:
+        new_piece.append(list(line))
+    tile = singleTile(new_piece)
+    usable_pieces.append(tile)
+
+problem = commonDivisorProblem(usable_pieces, usable_grid, 8)
+problem.print_all_tiles()
+problem.solve()
